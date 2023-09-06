c8ef003ebc745ef2dd587c499eeff3b73802a686
Efraín Martínez <a01280601@tec.mx>
2023-09-06 10:09:46 -0600

    Update listadoView.swift

--- caritas/listadoView.swift
+++ caritas/listadoView.swift
@@ -7,7 +7,7 @@ struct listadoView: View {
     @State private var filtroID: String = ""
     @State private var tipoFiltro: String = ""
     @State private var lista: Array<Recibo> = []
-
+//soy efragod scrum master
     
     
     var body: some View {

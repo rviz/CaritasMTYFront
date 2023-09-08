25f7acfa39790478906da16b6bbe2f60e32775bd
Adrian Cantu <A01284748@tec.mx>
2023-09-07 19:42:09 -0600

    intento de login

--- caritas/ModelData.swift
+++ caritas/ModelData.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+
 func callAPILista() -> Array<Recibo> {
     var lista: Array<Recibo> = []
         guard let url = URL(string:"https://jsonplaceholder.typicode.com/posts") else{
@@ -40,3 +41,51 @@ func callAPILista() -> Array<Recibo> {
     return lista
 }
 
+func prueba() -> User? {
+    let parameters = """
+    {
+        "username": "dummy",
+        "password": "contrasena"
+    }
+    """
+    
+    var user: User?
+    
+    if let postData = parameters.data(using: .utf8) {
+        var request = URLRequest(url: URL(string: "http://10.22.128.249:5000/collector/login")!, timeoutInterval: Double.infinity)
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = postData
+        
+        let group = DispatchGroup()
+        group.enter()
+
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            defer {
+                group.leave()
+            }
+            
+            guard let data = data else {
+                print(String(describing: error))
+                return
+            }
+            
+            do {
+                // Decodificar la respuesta JSON en un objeto User
+                let decodedUser = try JSONDecoder().decode(User.self, from: data)
+                user = decodedUser
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+            group.leave()
+        }
+
+        task.resume()
+        
+        group.wait()
+    } else {
+        print("Error encoding parameters")
+    }
+    
+    return user
+}

c7e7e903e8714980c98d828a01318de0497b0acc
Adrian Cantu <adriancantu1407@gmail.com>
2023-10-12 18:18:41 -0600

    Mensaje cuando no hay tickets con conflicto

--- caritas/ModelData.swift
+++ caritas/ModelData.swift
@@ -3,7 +3,7 @@ import Foundation
 
 func tickets(forManagerId managerId: Int) -> Array<ticket> {
     var lista: Array<ticket> = []
-    guard let url = URL(string: "http://10.22.175.143:5000/ticket/collector-tickets/\(managerId)") else {
+    guard let url = URL(string: "http://10.22.163.36:5000/ticket/collector-tickets/\(managerId)") else {
         print("Error: URL no válida")
         return lista
     }
@@ -37,7 +37,7 @@ func tickets(forManagerId managerId: Int) -> Array<ticket> {
 
 func recolectores() -> Array<Collector> {
     var lista: Array<Collector> = []
-    guard let url = URL(string: "http://10.22.175.143:5000/collector/get_by_manager_id/1") else {
+    guard let url = URL(string: "http://10.22.163.36:5000/collector/get_by_manager_id/1") else {
         print("Error: URL no válida")
         return lista
     }
@@ -74,7 +74,7 @@ func recolectores() -> Array<Collector> {
 }
 
 func InicioSesion(username: String, password: String, completion: @escaping (Int?, String?) -> Void) {
-    let url = URL(string: "http://10.22.175.143:5000/general/login")!
+    let url = URL(string: "http://10.22.163.36:5000/general/login")!
     var request = URLRequest(url: url)
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     request.setValue("application/json", forHTTPHeaderField: "Accept")
@@ -139,7 +139,7 @@ func InicioSesion(username: String, password: String, completion: @escaping (Int
 
 
 func CambiarComment(id: Int, comment: String, completion: @escaping (String?) -> Void) {
-    let url = URL(string: "http://10.22.175.143:5000/ticket/change_collector_comments")!
+    let url = URL(string: "http://10.22.163.36:5000/ticket/change_collector_comments")!
     var request = URLRequest(url: url)
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     request.setValue("application/json", forHTTPHeaderField: "Accept")
@@ -203,7 +203,7 @@ func CambiarComment(id: Int, comment: String, completion: @escaping (String?) ->
 
 
 func CambiarEstado(id: Int, state: String, completion: @escaping (String?) -> Void) {
-    let url = URL(string: "http://10.22.175.143:5000/ticket/change_state")!
+    let url = URL(string: "http://10.22.163.36:5000/ticket/change_state")!
     var request = URLRequest(url: url)
     request.setValue("application/json", forHTTPHeaderField: "Content-Type")
     request.setValue("application/json", forHTTPHeaderField: "Accept")
--- caritas/mgm_listadoView.swift
+++ caritas/mgm_listadoView.swift
@@ -1,187 +1,93 @@
-
-// RECOLECTOR
 import SwiftUI
 
 struct mgm_listadoView: View {
-    var id : Int
-    // Variables
-    @State private var option: Int = 1
-    @State private var filtroSearch: String = ""
-    @State private var tipoFiltro: String = ""
-    @State private var colorEstado: Color = .black
+    var id: Int
     @State private var lista: Array<ticket> = []
     @State var yaCargo: Bool = false
-    @State private var formattedDateString: String = ""
-    var displayedItemsCount: Int = 1
-    @State private var contadorConflictos: Int = 0
     
-    func isThereConflict( tickets: Array<ticket>) -> Bool {
-        var ret: Int = 0;
-        for t in tickets {
-            if t.state == "CONFLICT" {
-                ret += 1
-            }
-        }
-        
-        if(ret != 0){
-            return true
-        } else {
-            return false
-        }
+    func ticketsWithConflict() -> [ticket] {
+        return lista.filter {
+            $0.state == "CONFLICT" }
     }
     
     var body: some View {
-        NavigationStack{
-            VStack{
-                VStack{
+        NavigationStack {
+            VStack {
+                ZStack {
+                    // Barra de color
+                    Color(red: 0, green: 156/255, blue: 171/255)
+                        .edgesIgnoringSafeArea(.all)
+                        .frame(height: 80)
+                        .offset(y:-50)
                     
-                    ZStack{
-                        // Barra de color
-                        Color(red: 0, green: 156/255, blue: 171/255)
-                            .edgesIgnoringSafeArea(.all)
-                            .frame(height: 80)
-                            .offset(y:-50)
-                        
-                        Text("Tickets con conflicto")
-                            .font(.title)
-                            .fontWeight(.bold)
-                            .padding(.bottom, 5)
-                            .offset(y:-40)
-                            .foregroundColor(Color.white)
-
-                    }.offset(y:40)
-                }
+                    Text("Tickets con conflicto")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding(.bottom, 5)
+                        .offset(y:-40)
+                        .foregroundColor(Color.white)
+                }.offset(y:40)
                 
-                HStack{
-
-                    
-                    VStack() {
-                        ZStack{
-                            
-                            //  else{
-                            
-                            List {
-                                
-                                ForEach(Array(lista.enumerated()), id: \.1.id) { index, listaItem in
-                                    // Validación: Sólo se despliegan recibos en estado "conflict"
-                                    
-                                    if listaItem.state == "CONFLICT" {
+                VStack {
+                    if ticketsWithConflict().isEmpty {
+                        Image(systemName: "face.smiling.inverse")
+                            .resizable(resizingMode: .stretch)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 100)
+                            .foregroundColor(Color(red: 0, green: 156/255, blue: 171/255))
+                            .padding(.bottom, 20)
+                        
+                        Text("Este recolector no tiene recibos con conflicto.")
+                            .font(.title3)
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.center)
+                            .frame(width: 250)
+                    } else {
+                        List {
+                            ForEach(Array(lista.enumerated()), id: \.1.id) { _, listaItem in
+                                if listaItem.state == "CONFLICT" {
+                                    VStack(alignment: .leading, spacing: 5) {
+                                        let idReciboBD = listaItem.id == 0 ? "Sin información" : "\(String(listaItem.id))"
+                                        Text("ID: \(idReciboBD)")
+                                            .padding(.vertical, 3.0)
+                                            .padding(.horizontal, 11.0)
+                                            .background(Color(red: 0, green: 156/255, blue: 171/255))
+                                            .cornerRadius(8)
+                                            .foregroundColor(.white)
+                                            .font(.title3)
+                                            .padding(.top, 10)
                                         
-                                        VStack(alignment: .leading, spacing: 5) {
-                                            
-                                            
-                                            
-                                            VStack(alignment: .leading){
-                                                VStack {
-                                                    
-                                                    // Impresión de etiqueta "sin información"
-                                                    var idReciboBD: String {
-                                                        
-                                                        if listaItem.id == 0 {
-                                                            
-                                                            return "Sin información"
-                                                            
-                                                        } else {
-                                                            contadorConflictos = contadorConflictos + 1
-                                                            return "\(String(listaItem.id))"
-                                                        }
-                                                    }
-                                                    
-                                                    
-                                                    Text("ID: \(idReciboBD)")
-                                                        .padding(.vertical, 3.0)
-                                                        .padding(.horizontal, 11.0)
-                                                        .background(Color(red: 0, green: 156/255, blue: 171/255))
-                                                        .cornerRadius(8)
-                                                        .foregroundColor(.white)
-                                                        .font(.title3)
-                                                    
-                                                }.padding(.top, 10)
-                                                
-                                                
-                                                
-                                                VStack {
-                                                    // Impresión de etiqueta "sin información"
-                                                    var donanteBD: String {
-                                                        if listaItem.collectorComments.isEmpty {
-                                                            return "Sin información"
-                                                        } else {
-                                                            return listaItem.collectorComments
-                                                        }
-                                                    }
-                                                    
-                                                    Text("Comentarios: ")
-                                                        .fontWeight(.bold)
-                                                    +
-                                                    Text(donanteBD)
-                                                }.padding(.bottom, 10)
-                                                
-                                            }.font(.title3)
-                                            
-                                        }
-                                        
-                                        .padding(0)
+                                        let donanteBD = listaItem.collectorComments.isEmpty ? "Sin información" : listaItem.collectorComments
+                                        Text("Comentarios: ").fontWeight(.bold) + Text(donanteBD)
                                     }
-                                }
-                            }
-                            .refreshable{
-                                lista = tickets(forManagerId:id)
-                            }
-                            .onAppear() {
-                                if yaCargo==false{
-                                    lista = tickets(forManagerId: id)
-                                    yaCargo = true
-                                    
-                                }
-                            }
-                            .frame(width: 350, height: 515)
-                            .padding(.top, 15)
-                            .listStyle(.inset)
-                        }
-                        
-                        if (contadorConflictos == 0){
-
-                            Group{
-                                Image(systemName: "face.smiling.inverse")
-                                    .resizable(resizingMode: .stretch)
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: 100)
-                                    .foregroundColor(Color(red: 0, green: 156/255, blue: 171/255))
-                                    .padding(.bottom, 20)
-
-                                Text("Este recolector no tiene recibos con conflicto.")
+                                    .padding(.bottom, 10)
                                     .font(.title3)
-                                    .foregroundColor(.gray)
-                                    .multilineTextAlignment(.center)
-                                    .frame(width: 250)
-
-                            }.offset(y:-500)
+                                }
+                            }
                         }
-                       // }
-                            
-                            
-                        
-                        
+                        .refreshable {
+                            lista = tickets(forManagerId: id)
+                        }
+                        .onAppear {
+                            if !yaCargo {
+                                yaCargo = true
+                            }
+                        }
+                        .frame(width: 350, height: 515)
+                        .padding(.top, 15)
+                        .listStyle(.inset)
                     }
-                    
+                } .onAppear {
+                        lista = tickets(forManagerId: id)
                 }
                 Spacer()
-                
-                
             }
-            
         }
     }
 }
 
-
-
-
-
 struct mgm_listadoView_Previews: PreviewProvider {
     static var previews: some View {
-        
         mgm_listadoView(id: 1)
-
     }
 }
